b81fb165e350851d5a336546e5eba1901497d082
JumrahRusman <undaaa27@gmail.com>
2024-09-04 17:08:38 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,11 @@
+package unda;
+
+public class day3 {
+    public static void main(String []args){
+        System.out.println("nama jumrah");
+        System.out.println("umur 18");
+        System.out.println("alamat papalang");
+        
+        
+    }
+}
