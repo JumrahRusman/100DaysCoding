df246bf8a937c50dadf7471fe5d3a2ee41c724bc
JumrahRusman <undaaa27@gmail.com>
2024-09-08 22:59:23 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package undaCantik;
+
+public class day7 {
+    public static void main(String[]args){
+        int a, b, d;
+         a = 27;
+         b = 18;
+         b *= a;
+         d = (a - b);
+        
+        
+        System.out.println(b);
+        System.out.println(d);
+        
+    }
+    
+}
