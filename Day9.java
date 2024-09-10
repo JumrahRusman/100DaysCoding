6e1d33b5c67a3631531e08392e53269d4f5487c6
JumrahRusman <undaaa27@gmail.com>
2024-09-10 20:31:25 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+package undaaa;
+
+public class day9 {
+    public static void main(String[]args){
+        
+           // fariabel konstanta
+        final float nilai = 2.7f;
+        final int jumlah = 227;
+           // jumlah = 222;  (akan menyebabkan error)
+        
+      System.out.println("nilai :" + nilai);
+      System.out.println("jumlah :" + jumlah);  
+    }
+}
