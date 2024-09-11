4211c5f48357341a366e9863ec44b0d0ad04576d
JumrahRusman <undaaa27@gmail.com>
2024-09-11 20:10:48 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+package undaaaa;
+
+public class day10 {
+    public static void main(String[]args){
+      
+        byte nilaiMinimal = -128;
+        byte nilaiMaksimal = 127;
+      
+        System.out.println("Nilai minimal tipe data byte: " + nilaiMinimal);
+        System.out.println("Nilai maksimal tipe data byte: " + nilaiMaksimal);
+      
+    }
+    
+}
