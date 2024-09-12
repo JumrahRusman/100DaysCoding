42fd4ab6fb5698c69db72c10714517cbadcb822f
JumrahRusman <undaaa27@gmail.com>
2024-09-12 21:58:30 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,12 @@
+package day11;
+
+public class undaaaaa {
+    public static void main(String[]agrs){
+        short minimum = -32768;  
+        short maksimum = 32767;
+        
+        System.out.println("nilai minimum :" + minimum);
+        System.out.println("nilai maksimum:" + maksimum);
+    }
+    
+}
