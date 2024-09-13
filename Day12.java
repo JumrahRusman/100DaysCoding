c46bf3b4647e40191715b3ab4d4ad64155993deb
JumrahRusman <undaaa27@gmail.com>
2024-09-13 21:12:32 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,12 @@
+package day12;
+
+public class undaa {
+    public static void main(String[]args){
+        int umur = 18;
+        int tinggi = 156;
+        
+    System.out.println("umur :" + umur);
+    System.out.println("tinggi :" + tinggi);     
+    }
+    
+}
