ca65611584cde71ee19fe65768d2c2ed5f325a08
JumrahRusman <undaaa27@gmail.com>
2024-09-14 15:51:13 +0800

    Create Day13.java

---
+++ Day13.java
@@ -0,0 +1,12 @@
+package undaaa;
+
+public class day13 {
+    public static void main(String[]args){
+        int a = 27;
+            a = 18;
+        String b = " halo jumrah";
+        
+    System.out.println("umur :" + a);   
+    System.out.printf("%s", b);    
+    }
+}
