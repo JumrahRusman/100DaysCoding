01572f900338747a886183ecd3a1e65b59239248
JumrahRusman <undaaa27@gmail.com>
2024-09-15 21:44:53 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+package day14;
+
+public class undaaaaaa {
+    public static void main(String[]args){
+        long acak1= 760207670;
+        long acak2 = 46470101;
+        
+        System.out.println("acak1 :" + acak1);
+        System.out.println("acak2 :" + acak2);
+    }
+    
+}
