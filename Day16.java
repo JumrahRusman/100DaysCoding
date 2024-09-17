f51f82139c09a3338c93ae29ccfbcee7927b7f86
JumrahRusman <undaaa27@gmail.com>
2024-09-17 20:04:48 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,11 @@
+package undaaa;
+
+public class day16 {
+    public static void main(String[]args){
+        float acak1 = 30.4f;
+        float acak2 = 27.4f;
+        System.out.println(acak1 + acak2);  
+        
+    }
+    
+}
