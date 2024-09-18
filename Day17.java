7e4d0101bb14c8d608a4052c409e60096ecdd8df
JumrahRusman <undaaa27@gmail.com>
2024-09-18 20:30:01 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+package unda;
+public class day17 {
+    public static void main(String[]args){
+        char  a, b, c, d;
+        a = 'u';
+        b = 'n';
+        c = 'd';
+        d = 'a';
+        System.out.println("nama" +a+b+c+d);
+        
+    }
+}
