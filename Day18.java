267023bf8fcf380ea447412f0a1f7404dd0ac84b
JumrahRusman <undaaa27@gmail.com>
2024-09-19 19:55:25 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,14 @@
+package unda;
+public class day17 {
+    public static void main(String[]args){
+        // tipe data boolean
+        int a, b;
+         a = 27;
+         b = 40;
+        boolean c = a > b;
+        boolean d = a < b;
+        
+        System.out.println(c); // fals
+        System.out.println(d); //true
+    }
+    }
