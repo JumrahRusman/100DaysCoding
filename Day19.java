ea5af02851a2d43873fe72a0238e121c82927fc0
JumrahRusman <undaaa27@gmail.com>
2024-09-20 22:20:50 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,9 @@
+package day19;
+public class undaaaaaaaaa {
+    public static void main(String[]args){
+        String a = "nama";
+        String b = "jumrah";
+        System.out.println(a+":"+ b);
+        
+    }
+}
