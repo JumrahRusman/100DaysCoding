2732ccf86d491c27fdbadb062f32ac6c15d9f15b
JumrahRusman <undaaa27@gmail.com>
2024-09-21 21:53:11 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,9 @@
+public class day20 {
+    public static void main(String[]args){
+        Byte a = 25;
+        Byte b = 12;
+        
+        System.out.println("hasil: " +a);
+        System.out.println("hasil: " +b);
+    }
+}
