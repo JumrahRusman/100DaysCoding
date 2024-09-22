666e442253d61e6cc502f5af78565cb91acc0c04
JumrahRusman <undaaa27@gmail.com>
2024-09-22 23:53:21 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,8 @@
+public class day21 {
+    public static void main(String[]args){
+        Short a = 123;
+        Short b = 321;
+        System.out.println("nilai :" + a);
+        System.out.println("nilai :" + b);
+    }
+}
