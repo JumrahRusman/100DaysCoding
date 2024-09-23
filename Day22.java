236547d131269051ff489aefdb86dfcfac993168
JumrahRusman <undaaa27@gmail.com>
2024-09-23 21:55:32 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,12 @@
+package undaaa;
+
+public class day22 {
+    public static void main(String[]args){
+        Integer a = 5000;
+        Integer b = 2000;
+        
+        System.out.println("harga nasi :" + a);
+        System.out.println("harga bakwan :" + b);
+    } 
+    
+}
