36f670e72cbd19d60dfa36dd885c70a5f8ed50d5
JumrahRusman <undaaa27@gmail.com>
2024-09-24 23:40:03 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class day23 {
+    public static void main(String[]args){
+        Long a = 1996L;
+        Long b = 3456L;
+        System.out.println("hasil a :" +a);
+        System.out.println("hasil b :" +b);
+        
+    }
+    
+}
