9c64dcf532d918a3db4f5461fe474cc4ba8591b6
JumrahRusman <undaaa27@gmail.com>
2024-09-25 22:20:31 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,9 @@
+public class day24 {
+    public static void main(String[]args){
+        Float bb = 43.5f;
+        Float tb = 155.5f;
+        System.out.println("berat badan  :" + bb);
+        System.out.println("tinggi badan :" + tb);
+    }
+}
+    
