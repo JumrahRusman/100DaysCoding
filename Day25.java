542ce4b6e62e56045c7d62a31dfe469138ae6c15
JumrahRusman <undaaa27@gmail.com>
2024-09-26 23:00:38 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,9 @@
+public class day25Java {
+    public static void main(String[]args){
+        Double a = 27.5;
+        Double b = 63.8;
+        System.out.println("nilai a : " + a);
+        System.out.println("niali b : " + b);
+    }
+    
+}
