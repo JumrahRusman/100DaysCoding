b90ccfd2278b3db3a0baba32f36f13f517d19132
JumrahRusman <undaaa27@gmail.com>
2024-09-27 22:56:37 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,18 @@
+public class day26 {
+    public static void main(String[]args){
+        Character a,b,c,d,e,f;
+        a = 'j';
+        b = 'u';
+        c = 'm';
+        d = 'r';
+        e = 'a';
+        f = 'h';
+        System.out.print("nama:" +a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.print(e);
+        System.out.print(f);
+    } 
+    
+}
