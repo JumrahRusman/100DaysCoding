72330272e364a87e30c77ee98ef118e3a5ffec60
JumrahRusman <undaaa27@gmail.com>
2024-09-29 21:20:00 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package day28.java;
+
+public class undha {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 5;
+        Boolean c = a>b;
+        System.out.print(c); //true
+    }
+}
