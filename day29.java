c4007ed4df8139455feca6836e6766e9b1b1cf31
JumrahRusman <undaaa27@gmail.com>
2024-09-30 21:56:27 +0800

    day29.java

---
+++ day29.java
@@ -0,0 +1,14 @@
+package day29;
+import java.math.BigInteger; 
+public class undaaa {
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger ("1");
+        BigInteger b = new BigInteger ("2");
+        
+        //aritmatika 
+        BigInteger jumlah = a.add(b);
+        System.out.println(jumlah);
+        
+    }
+}
+ 
