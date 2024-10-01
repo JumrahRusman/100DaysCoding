a4178a411dd93623670ec374cc7b619b28da2d36
JumrahRusman <undaaa27@gmail.com>
2024-10-01 23:39:59 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,11 @@
+package unda;
+import java.math.BigDecimal;
+public class day30 {
+    public static void main(String[]args){
+        BigDecimal a = new BigDecimal(1.1);
+        BigDecimal b = new BigDecimal(2.2);
+        BigDecimal jumlah = a.add(b);
+        System.out.print("hasil penjumlahan dari a dan b :" + jumlah );
+    }
+    
+}
