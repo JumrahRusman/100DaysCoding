5970aecb7e76dfe380328f8bf46bb0690b236f71
JumrahRusman <undaaa27@gmail.com>
2024-10-02 22:45:38 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,8 @@
+package Day31.java;
+public class Main {
+    public static void main(String[] args) {
+       String tipe_integer ="27";
+       int x = Integer.parseInt(tipe_integer); 
+        System.out.println("hasil :" + x);
+    }  
+}
