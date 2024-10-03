2462f37c20584666f748bd26770bd1817cc7a62c
JumrahRusman <undaaa27@gmail.com>
2024-10-03 21:29:38 +0800

    Create day32.java

---
+++ day32.java
@@ -0,0 +1,22 @@
+package day32.java;
+public class jumrah {
+    public static void main(String[] args) {
+        //bilangan bulat
+        byte a = 18;
+        String unda = Byte.toString(a);
+        System.out.println(unda);
+        
+        short b = 27;
+        String undaa = Short.toString(b);
+        System.out.println(undaa);
+        
+        int c = 100;
+        String undaaa = Integer.toString(c);
+        System.out.println(undaaa);
+        
+        long d = 127;
+        String undaaaa = Long.toString(d);
+        System.out.println(undaaaa); 
+    }
+    
+}
