9087b2aab90580d14aebfcce61e212563c4bcfbb
JumrahRusman <undaaa27@gmail.com>
2024-10-06 21:27:30 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("halo selamat malam");
+        //penjumlahan
+        byte a = 10;
+        short  b = 20;
+        byte hasil = (byte) (a+b);
+        System.out.printf("hasil %d",hasil);
+        
+        //pemgurangan 
+         int d = 30;
+         long e = 40;
+         int hasil1 = (int) (d - e);
+        System.out.printf("hasil %d", hasil1);
+        
+        
+        
+        
+        
+    }
+    
+}
