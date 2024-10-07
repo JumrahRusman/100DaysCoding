b61976c19c0376da112376daee9a60289eeb59d9
JumrahRusman <undaaa27@gmail.com>
2024-10-07 21:54:37 +0800

    Create day36.java

---
+++ day36.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class day36 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("bilangan pertama : ");
+        int a = input.nextInt();
+        System.out.print("bilangan  kedua : ");
+        int b = input.nextInt();
+        System.out.print("bilangan ketiga : ");
+        int c = input.nextInt();
+        
+        int hasilKali = a * b * c;
+        System.out.println("hasil perkalian = " + hasilKali);
+        int hasilBagi = a / b / c;
+        System.out.println("hasil bagi = " + hasilBagi);
+        int hasilSisaBagi = a % b % c;
+        System.out.println("hasilSisaBagi = " + hasilSisaBagi);
+    }
+    
+}
