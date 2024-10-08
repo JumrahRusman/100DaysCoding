5ba7a3eaceee296221319efc1386e29785ab0baf
JumrahRusman <undaaa27@gmail.com>
2024-10-08 20:29:30 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,17 @@
+public class day37 {
+    public static void main(String[]args){
+        int a;
+        a = 3; // output 3
+        System.out.println("nilai : " +a); 
+         
+        a += 3; // outpot 6
+        System.out.println("nilai  : " +a); 
+        
+        a -= 3; // outpot 3
+        System.out.println("nilai  : " +a); 
+        
+        
+        
+    }
+    
+}
