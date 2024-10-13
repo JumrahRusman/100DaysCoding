4361edb9384e7dd41815d6732328eee3e8578db3
JumrahRusman <undaaa27@gmail.com>
2024-10-14 00:36:12 +0800

    Create day43.java

---
+++ day43.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class day42 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+   
+        System.out.print("masukkan nilai = ");
+        int j = input.nextInt();
+       
+        
+        if (j >= 80){
+            System.out.println ("mencapai target");
+        }else if(j <= 80){
+            System.out.println ("nilai dibawah target");
+            
+  
+          }
+        }    
+    }
+    
+
