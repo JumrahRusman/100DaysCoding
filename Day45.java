bfe32606d890d23b7d2192e3771a1e3287d8aca5
JumrahRusman <undaaa27@gmail.com>
2024-10-16 23:35:47 +0800

    Create Day45.java

---
+++ Day45.java
@@ -0,0 +1,11 @@
+public class day45 {
+    public static void main(String[]args){
+        int a, b;
+        a = 25;
+        b = 27;
+         
+        System.out.println(a > b || b < a); //false
+        System.out.println(a < b || b > a); //true
+    }
+    
+}
