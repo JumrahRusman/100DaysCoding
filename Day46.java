7682c87aa6c018f7ca602a8e3810baac586b709f
JumrahRusman <undaaa27@gmail.com>
2024-10-17 22:32:43 +0800

    Create Day46.java

---
+++ Day46.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day46 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("nilai a = ");
+        boolean a = input.nextBoolean();
+        System.out.print("masukkan nilai a = " + !a);
+        
+}
+}
