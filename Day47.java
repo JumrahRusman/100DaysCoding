b1848343d085f7be99d17b9d3ede9b7b11a5008f
JumrahRusman <undaaa27@gmail.com>
2024-10-18 23:19:27 +0800

    Create Day47.java

---
+++ Day47.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class day47 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("nilai a = ");
+        int a = input.nextInt();
+        
+        
+        if(a > 0 ){
+            System.out.println("positif");
+            
+        }
+        }
+        
+}
