514a472ecc5f69e3aacb385b55156285ae0ae700
JumrahRusman <undaaa27@gmail.com>
2024-10-25 22:15:41 +0800

    Create day54.java

---
+++ day54.java
@@ -0,0 +1,18 @@
+
+package day54;
+
+import java.util.Scanner;
+
+public class unda {
+    public static void main(String[] args) {
+//membuat angka genap
+    Scanner input = new Scanner(System.in);
+        System.out.print("masukkan nilai : ");
+        int akhir = input.nextInt();
+        
+        for(int mulai = 30; mulai >  akhir ; mulai -=2){
+        System.out.println( mulai);
+
+        }
+    }
+}
