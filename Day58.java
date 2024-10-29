af5d56ca6ae5bf698e7593f9cf992fbbdf4b9c39
JumrahRusman <undaaa27@gmail.com>
2024-10-29 22:39:20 +0800

    Create Day58.java

---
+++ Day58.java
@@ -0,0 +1,13 @@
+public class day58 {
+    public static void main(String[]args){
+        
+        int a = 10;
+        while(a > 1){
+            a--;
+            System.out.print(" " + a);
+        }
+        
+        
+    }
+    
+}
