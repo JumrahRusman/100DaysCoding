9065f649d3b99b4515a1a9c317ae4807df0de103
JumrahRusman <undaaa27@gmail.com>
2024-11-03 21:04:23 +0800

    Create Day63.java

---
+++ Day63.java
@@ -0,0 +1,21 @@
+package day63;
+
+import java.util.Scanner;
+
+public class undaaa {
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+      
+        System.out.print("masukkan angka terakhir = ");
+        int b = input.nextInt();
+        int a;
+        for(a = 1; a<b; a++){
+            if(a%2==0){
+                System.out.println("bozz");
+                continue;
+            }
+            System.out.println(a); 
+        
+    }System.out.println(a);
+    }
+}
