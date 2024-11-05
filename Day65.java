524411cba32a4b22eee8175aeb7ce72727c60ec9
JumrahRusman <undaaa27@gmail.com>
2024-11-05 22:00:27 +0800

    Create Day65.java

---
+++ Day65.java
@@ -0,0 +1,10 @@
+public class day65 {
+    public static void main(String[]args){
+        for(int a = 1; a < 3; a++){
+             for(int b = 1; b < 3; b++){
+                 System.out.println("luar "+ a +" dalam " + b);
+    }
+    }
+        System.out.println("selesai");
+}
+}
