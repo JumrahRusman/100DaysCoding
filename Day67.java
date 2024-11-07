63586d24cb72023c9c7d20de28323306c1cbfc20
JumrahRusman <undaaa27@gmail.com>
2024-11-07 22:14:48 +0800

    Create Day67.java

---
+++ Day67.java
@@ -0,0 +1,10 @@
+
+public class day67 {
+    public static void cetakPesan() {
+        System.out.println("jumrahhhh");
+    }
+
+    public static void main(String[] args) {
+        cetakPesan();
+    }
+}
