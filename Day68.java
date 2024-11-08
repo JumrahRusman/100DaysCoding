70785fb2689e65e7023e62c6b2b22ed6b9b355a6
JumrahRusman <undaaa27@gmail.com>
2024-11-08 23:42:51 +0800

    Create Day68.java

---
+++ Day68.java
@@ -0,0 +1,13 @@
+package day68;
+public class undaa {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        int hasil = tambah(a,b);
+        System.out.println(a+ "+" +b+ "="+ hasil);
+    }
+      static int tambah(int x,int y){
+          return x + y;
+      }
+}
