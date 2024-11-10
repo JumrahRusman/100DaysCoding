afb73c92664894d54e987efe44489cf21d690bb3
JumrahRusman <undaaa27@gmail.com>
2024-11-10 19:32:01 +0800

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,10 @@
+public class day70 {
+    
+    public static String loppy (String nama,int umur){
+        return nama + " berumur "+umur +" tahun";
+    }
+    public static void main(String[] args) {
+        System.out.println(loppy("unda",18));
+    }
+    
+}
