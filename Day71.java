ba517248cb9337193d76b251ceff3aa334910860
JumrahRusman <undaaa27@gmail.com>
2024-11-11 23:22:30 +0800

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,11 @@
+package day71;
+public class unda {
+    public static void main(String[] args) {
+        System.out.println("aloooo!!");
+     System.out.println(nonMain(5));
+           }
+          public static int nonMain(int a) {
+              return a;
+    }
+    
+}
