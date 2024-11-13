325c429b615c2e67c1e21fa195eb094063be18fc
JumrahRusman <undaaa27@gmail.com>
2024-11-13 22:24:41 +0800

    Create Day73.java

---
+++ Day73.java
@@ -0,0 +1,10 @@
+public class day73 {
+    public static void main(String[]args){
+        String nama = "jumrah";
+        
+        System.out.print(nama);
+        System.out.print(" char[3] adalah "+nama.charAt(3));
+        
+    }
+    
+}
