549a1d6530b49296e3206302ef572a05ed45e746
JumrahRusman <undaaa27@gmail.com>
2024-11-14 20:53:37 +0800

    Create Day74.java

---
+++ Dau74.java
@@ -0,0 +1,11 @@
+public class day74 {
+    public static void main(String[]args){
+        String nama = "jumrah undaa";
+        
+        System.out.println(nama);
+        System.out.print("jumlah karakter : " + nama.length());
+        
+        
+    }
+    
+}
