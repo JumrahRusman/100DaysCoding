4920adc7fdd07e1e9d8e386b81cb318b58d75a6b
JumrahRusman <undaaa27@gmail.com>
2024-11-15 21:25:44 +0800

    Create Day75.java

---
+++ Day75.java
@@ -0,0 +1,10 @@
+public class day75 {
+    
+    public static void main(String[] args) {
+        String nama = "JUMRAH";
+        
+        System.out.println(nama.toLowerCase());
+    }
+    
+}
+
