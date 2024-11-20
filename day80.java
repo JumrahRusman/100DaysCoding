c16850443f38f2d560092698c657fe1de8b83cc2
JumrahRusman <undaaa27@gmail.com>
2024-11-20 21:28:50 +0800

    Create day80.java

---
+++ day80.java
@@ -0,0 +1,8 @@
+public class day80 {
+    public static void main(String[] args) {
+        String a = "\t Alloooow guys!!!! \t";
+
+        System.out.println(a); //sebelum outputnya (    Alloooow guys!!!!    )
+        System.out.println(a.trim()); //sesudah  (Alloooow guys!!!!)
+    }
+}
