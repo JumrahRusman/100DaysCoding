19ca0f844cc7496f480eb7c1c231c79db5ec9cf3
JumrahRusman <undaaa27@gmail.com>
2024-12-09 07:01:10 -0800

    Create day99.java

---
+++ day99.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+public class day99 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+
+                System.out.print("masukkan pertama: ");
+                int a = sc.nextInt();
+
+                System.out.print("masukkan kedua: ");
+                int b = sc.nextInt();
+
+                System.out.print("pilih operasi (+, -, *, /): ");
+                char operasi = sc.next().charAt(0);
+
+                int hasil = 0;
+                switch (operasi) {
+                    case '+':
+                        hasil = a + b;
+                        break;
+                    case '-':
+                        hasil = a - b;
+                        break;
+                    case '*':
+                        hasil = a * b;
+                        break;
+                    case '/':
+                        if (b != 0) {
+                            hasil = a / b;
+                        } else {
+                            System.out.println("tidak di temukan");
+                            return;
+                        }
+                        break;
+                    default:
+
+                        return;
+                }
+
+                System.out.println("Hasil: " + hasil);
+            }
+        }
+
+    }
+
